f0aaf432416c9217dd4338815400802d59d023fd
Jim Cooke <Jim.Cooke@coogee.email>
2019-01-27 15:02:58 +1100

    Fix incorrect CI/CD secret variables

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,9 +5,9 @@ if [ $FILES_TO_MOVE -gt 0 ]; then
   apt-get -y install git-core
   git config --global user.email 'jim.cooke.aus@gmail.com'
   git config --global user.name 'jimcooke'
-  git remote set-url origin https://$GIT_CI_USER:$GIT_CI_PASS@gitlab.com/$CI_PROJECT_PATH.git
-  git clone https://$GIT_CI_USER:$GIT_CI_PASS@gitlab.com/$CI_PROJECT_PATH.git &> /dev/null
-  cd $GIT_CI_ROOT
+  git remote set-url origin https://${GIT_CI_USER}:${GIT_CI_PASS}@gitlab.com/${GIT_CI_PROJECT_PATH}.git
+  git clone https://${GIT_CI_USER}:${GIT_CI_PASS}@gitlab.com/${GIT_CI_PROJECT_PATH}.git &> /dev/null
+  cd ${GIT_CI_ROOT}
 
   # We need to remove any old patch versions for this <upgrade>.<feature> version
   FEATURE_VERSION=$(cat VERSION | cut -f1-2 -d '.')
@@ -20,7 +20,7 @@ if [ $FILES_TO_MOVE -gt 0 ]; then
   git add --all
   git commit -m "GitLab Runner Push"
   git tag -a v$(cat VERSION) -m "$(date +'%d/%m/%Y') Copyright ©$(date +'%Y') by Jim Cooke"
-  git push https://$GIT_CI_USER:$GIT_CI_PASS@gitlab.com/$CI_PROJECT_PATH.git HEAD:master
+  git push https://${GIT_CI_USER}:${GIT_CI_PASS}@gitlab.com/${GIT_CI_PROJECT_PATH}.git HEAD:master
 else
   echo "Warning: No staging files were actually deployed"
   exit 1
